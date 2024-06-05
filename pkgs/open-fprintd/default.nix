7450d6da33d6e8105661d9c0f726c9f5a7097cc1
Benjamin Manuel <ben@benmanuel.com>
2024-06-04 19:24:02 -0500

    Update native build inputs for open-fprintd after python update

--- pkgs/open-fprintd/default.nix
+++ pkgs/open-fprintd/default.nix
@@ -1,67 +1,70 @@
 {
   pkgs,
   lib,
-  stdenv,
   fetchFromGitHub,
   python3Packages,
-  substituteAll
-}:
-
-let
+}: let
   inherit (python3Packages) python buildPythonPackage;
-in buildPythonPackage rec {
-  pname = "open-fprintd";
-  version = "0.6";
+in
+  buildPythonPackage rec {
+    pname = "open-fprintd";
+    version = "0.6";
 
-  src = fetchFromGitHub {
-    owner = "uunicorn";
-    repo = pname;
-    rev = "${version}";
-    sha256 = "sha256-uVFuwtsmR/9epoqot3lJ/5v5OuJjuRjL7FJF7oXNDzU="; # set to lib.fakeSha256 first to get the hash
-  };
+    src = fetchFromGitHub {
+      owner = "uunicorn";
+      repo = pname;
+      rev = "${version}";
+      sha256 = "sha256-uVFuwtsmR/9epoqot3lJ/5v5OuJjuRjL7FJF7oXNDzU="; # set to lib.fakeSha256 first to get the hash
+    };
 
-  patches = [
-    ./setup.py.patch
-    ./scripts.patch
-  ];
+    nativeBuildInputs = [
+      pkgs.gobject-introspection
+      pkgs.wrapGAppsHook3
+      pkgs.python3Packages.wrapPython
+    ];
 
-  postPatch = ''
-    # We now edit in nix store paths where necessary
+    patches = [
+      ./setup.py.patch
+      ./scripts.patch
+    ];
 
-    substituteInPlace debian/open-fprintd.service \
-      --replace "ExecStart=/usr/lib/open-fprintd/open-fprintd" \
-                "ExecStart=$out/bin/open-fprintd"
+    postPatch = ''
+      # We now edit in nix store paths where necessary
 
-    substituteInPlace debian/open-fprintd-suspend.service \
-      --replace "ExecStart=/usr/lib/open-fprintd/suspend.py" \
-                "ExecStart=$out/bin/open-fprintd-suspend.py"
+      substituteInPlace debian/open-fprintd.service \
+        --replace "ExecStart=/usr/lib/open-fprintd/open-fprintd" \
+                  "ExecStart=$out/bin/open-fprintd"
 
-    substituteInPlace debian/open-fprintd-resume.service \
-      --replace "ExecStart=/usr/lib/open-fprintd/resume.py" \
-                "ExecStart=$out/bin/open-fprintd-resume.py"
-  '';
+      substituteInPlace debian/open-fprintd-suspend.service \
+        --replace "ExecStart=/usr/lib/open-fprintd/suspend.py" \
+                  "ExecStart=$out/bin/open-fprintd-suspend.py"
 
-  propagatedBuildInputs = with python3Packages; [
-    dbus-python
-    pygobject3
-  ];
+      substituteInPlace debian/open-fprintd-resume.service \
+        --replace "ExecStart=/usr/lib/open-fprintd/resume.py" \
+                  "ExecStart=$out/bin/open-fprintd-resume.py"
+    '';
 
-  postInstall = ''
-    # this section has been adapted from this AUR package: https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=open-fprintd
+    propagatedBuildInputs = with python3Packages; [
+      dbus-python
+      pygobject3
+    ];
 
-    install -D -m 644 debian/open-fprintd.service \
-      $out/lib/systemd/system/open-fprintd.service
+    postInstall = ''
+      # this section has been adapted from this AUR package: https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=open-fprintd
 
-    install -D -m 644 debian/open-fprintd-resume.service \
-      $out/lib/systemd/system/open-fprintd-resume.service
+      install -D -m 644 debian/open-fprintd.service \
+        $out/lib/systemd/system/open-fprintd.service
 
-    install -D -m 644 debian/open-fprintd-suspend.service \
-      $out/lib/systemd/system/open-fprintd-suspend.service
-  '';
+      install -D -m 644 debian/open-fprintd-resume.service \
+        $out/lib/systemd/system/open-fprintd-resume.service
 
-  meta = with lib; {
-    description = "Fprintd replacement which allows you to have your own backend as a standalone service";
-    homepage = "https://github.com/uunicorn/open-fprintd";
-    license = licenses.gpl2;
-  };
-}
+      install -D -m 644 debian/open-fprintd-suspend.service \
+        $out/lib/systemd/system/open-fprintd-suspend.service
+    '';
+
+    meta = with lib; {
+      description = "Fprintd replacement which allows you to have your own backend as a standalone service";
+      homepage = "https://github.com/uunicorn/open-fprintd";
+      license = licenses.gpl2;
+    };
+  }
